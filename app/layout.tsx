4b289989a47bdcd53bee2952d8839db9f21fe520
VladimirBaykov <benworkens@gmail.com>
2026-06-18 16:10:35 +0300

    Use English dates on Journal cards

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import { Header } from "@/components/header/Header";
 import { JournalProvider } from "@/components/journal/JournalContext";
 import { JournalMenuRuntimeGuard } from "@/components/journal/JournalMenuRuntimeGuard";
 import { JournalLockSecurityGuard } from "@/components/journal/JournalLockSecurityGuard";
+import { JournalEnglishDateGuard } from "@/components/journal/JournalEnglishDateGuard";
 import { ToastProvider } from "@/components/ui/ToastContext";
 import { AnimatedLayout } from "@/components/layout/AnimatedLayout";
 import { BottomNav } from "@/components/navigation/BottomNav";
@@ -53,6 +54,7 @@ export default function RootLayout({
             <JournalProvider>
               <JournalMenuRuntimeGuard />
               <JournalLockSecurityGuard />
+              <JournalEnglishDateGuard />
               <Header />
 
               <main className={mainClassName}>
